d41c6248e002f260653d7f2d6736909b9e819374
Brambor <tondadrd@seznam.cz>
2017-02-15 22:47:15 +0100

    tomova funkce

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	echo
-	'<img src="16776155_1761208477238564_692518005_o.png" alt="Planets" usemap="#parlerovka">
+	'<img src="16776155_1761208477238564_692518005_o.png" usemap="#parlerovka">
 	<map name="parlerovka">
-		<area shape="rect" coords="699,354,935,728" href="room.php?room=hala" alt="Sun">
+		<area shape="rect" coords="699,354,935,728" href="room.php?rooms=hala&date=15-2-2017">
 	</map>'
 ?>
